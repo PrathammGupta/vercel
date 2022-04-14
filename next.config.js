eb3e97d1b0101a53356bdb37e6d5bdf852b61ab8
PrathammGupta <74899461+PrathammGupta@users.noreply.github.com>
2022-04-14 13:47:54 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -6,4 +6,20 @@ module.exports = {
     ignoreDuringBuilds: true,
   },
 }
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+const withPWA = require("next-pwa");
 
+module.exports = withPlugins(
+    [optimizedImages],
+    [
+      withPWA({
+        pwa: {
+          dest: "public",
+        },
+      }),
+    ],
+    {
+      distDir: '../../dist/client',
+    }
+);
